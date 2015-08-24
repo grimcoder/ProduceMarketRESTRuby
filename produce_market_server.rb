2e07902118cf57eae200cd1af008526069fe1dd5
grimcoder <papochka12>
2015-08-24 01:22:10 -0700

    implemented sales for in-memory

--- produce_market_server.rb
+++ produce_market_server.rb
@@ -8,7 +8,17 @@ class ProduceMarketServer
     file = File.read('./data/prices.json')
     data_hash = JSON.parse(file)
     @prices = data_hash
+
+    file = File.read('./data/sales.json')
+    data_hash = JSON.parse(file)
+    @sales = data_hash
+
+    file = File.read('./data/priceChanges.json')
+    data_hash = JSON.parse(file)
+    @priceChanges = data_hash
   end
+
+
   def getPrices
     return @prices
   end
@@ -16,10 +26,33 @@ class ProduceMarketServer
   def getPrice(i)
     return @prices.select { |p| p['Id'].to_s == i }
   end
+
+  def getSales
+    return @sales
+  end
+
+  def getSale(i)
+    return @sales.select { |p| p['Id'].to_s == i }
+  end
+
+
+  def getPriceChanges
+    return @priceChanges
+  end
+
+
+
 end
 
 server = ProduceMarketServer.new
 set :port, 3001
+
+# set CORS
+before do
+  # content_type :json
+  headers 'Access-Control-Allow-Origin' => '*',
+          'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST']
+end
 
 get '/api/prices' do
   return_message = {}
@@ -31,9 +64,18 @@ get '/api/prices' do
   return_message.to_json
 end
 
-# set CORS
-before do
-  content_type :json
-  headers 'Access-Control-Allow-Origin' => '*',
-          'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST']
+get '/api/sales' do
+  return_message = {}
+  if params.has_key?('id')
+    return_message = server.getSale(params['id'])
+  else
+    return_message = server.getSales
+  end
+  return_message.to_json
+end
+
+get '/api/priceChanges' do
+
+  return_message = server.getPriceChanges
+  return_message.to_json
 end
