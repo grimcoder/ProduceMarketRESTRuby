f73684d2571fcbb82719a508e610ee6da9a03c53
grimcoder <papochka12>
2015-08-24 22:47:02 -0700

    finished in - memory db

--- app.rb
+++ app.rb
@@ -43,18 +43,48 @@ class ProduceMarketServer
   end
 
   def updatePrice(ng_params)
-    # code here
+    oldprice = @prices.find {|i| i['Id'] == ng_params['Id']}
+    pricewas = oldprice['Price']
+    @prices = @prices.reject {|i| i['Id'] == ng_params['Id']}
+    @prices << ng_params
+    tmp = ng_params.clone
+    action = 'Edit'
+    tmp['Action'] = 'Edit'
+    tmp["priceWas"] = oldprice['Price']
+    @priceChanges << tmp
+  end
+
+  def deletePrice(id)
+    oldprice = @prices.find {|i| i['Id'].to_s == id}
+    @prices = @prices.reject {|i| i['Id'].to_s == id}
+    oldprice = oldprice.clone
+    oldprice['Action'] = 'Delete'
+    oldprice['priceWas'] = 'n/a'
+    @priceChanges << oldprice
+  end
+
+
+  def deleteSale(id)
+    oldprice = @sales.find {|i| i['Id'].to_s == id}
+    @sales = @sales.reject {|i| i['Id'].to_s == id}
+
   end
 
   def createPrice(ng_params)
     newId = @prices.max{|a,b| a['Id'] <=> b['Id']}['Id'] + 1
     ng_params['Id'] = newId
     @prices << ng_params
-
+    tmp = ng_params.clone
+    tmp['Action'] = 'New'
+    tmp["priceWas"] = 'n/a'
+    @priceChanges << tmp
   end
 
   def updateSale(ng_params)
-    # code here
+
+    oldSale = @sales.find {|i| i['Id'] == ng_params['Id']}
+    @sales = @sales.reject {|i| i['Id'] == ng_params['Id']}
+    @sales << ng_params
   end
 
   def createSale(ng_params)
@@ -67,17 +97,17 @@ end
 
 server = ProduceMarketServer.new
 set :port, 3001
-
 # set CORS
+
 before do
   content_type :json
   headers 'Access-Control-Allow-Origin' => '*',
-          'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST']
+          'Access-Control-Allow-Methods' => %w(OPTIONS,GET,POST,DELETE,PUT)
 end
 
-options "*" do
-  response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
-  response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
+options '*' do
+  response.headers['Allow'] = 'HEAD,GET,PUT,POST,DELETE,OPTIONS'
+  response.headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept'
   200
 end
 
@@ -87,6 +117,22 @@ get '/api/prices' do
     return_message = server.getPrice(params['id'])
   else
     return_message = server.getPrices
+  end
+  return_message.to_json
+end
+
+delete '/api/prices' do
+  return_message = {}
+  if params.has_key?('id')
+    server.deletePrice(params['id'])
+  end
+  return_message.to_json
+end
+
+delete '/api/sales' do
+  return_message = {}
+  if params.has_key?('id')
+    server.deleteSale(params['id'])
   end
   return_message.to_json
 end
@@ -101,32 +147,27 @@ get '/api/sales' do
   return_message.to_json
 end
 
-get '/api/priceChanges' do
+get '/api/reports/prices' do
   return_message = server.getPriceChanges
   return_message.to_json
 end
 
 post('/api/prices') do
   ng_params = JSON.parse(request.body.read)
-
   if ng_params.has_key?('Id') #update
     server.updatePrice(ng_params)
   else #create
     server.createPrice(ng_params)
   end
-
   return "success".to_json
 end
 
 post('/api/sales') do
   ng_params = JSON.parse(request.body.read)
-
-
   if ng_params.has_key?('Id')
     server.updateSale(ng_params)
   else
     server.createSale(ng_params)
   end
   return "success".to_json
-
 end
